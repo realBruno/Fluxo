617c5a48bb709b16ef491e7399300a7a5a8ff1dc
Bruno <soubruno@protonmail.com>
2026-01-17 20:39:02 -0300

    refactor: fix byte_string static method and other methods too

--- src/client/bencode_parser.py
+++ src/client/bencode_parser.py
@@ -1,34 +1,29 @@
 from stack import Stack
 
-class BencodeParser:
-    """
-        Byte string and integer methods are like base cases;
-        List and dictionary methods are like recursive steps
-    """
+MAX_PRINTABLE = 127 # maximum possible decimal value for a printable char in the ASCII table
 
+class BencodeParser:
     stack = Stack()
     main_list = []
     main_dictionary = {}
-    is_key = True
-    key = ""
 
     @staticmethod
     def byte_string(file_content, index):
-        index += 1
         length = ""
         while (char := file_content[index]).isnumeric():
             length += char
             index += 1
 
         length = int(length)
-        index += 1  # so it skips character ':'
+
+        index += 1 # so it skips :
 
         string = ""
         while length > 0:
             string += file_content[index]
             length -= 1
             index += 1
-        return [string, index + 1]
+        return [string, index]
 
     @staticmethod
     def integer(file_content, index):
@@ -37,36 +32,32 @@ class BencodeParser:
         while (char := file_content[index]).isnumeric():
             integer += char
             index += 1
-        return [int(integer), index + 1]
+        return [int(integer), index]
 
     @staticmethod
-    def dictionary_or_list(file_content, index):
+    def structure(file_content, index):
+        print("Structure")
+        """To use with lists or dictionaries"""
         index += 1
         match file_content[index]:
-            case 'd', 'l': return BencodeParser.dictionary_or_list(file_content, index)
+            case 'd', 'l': return BencodeParser.structure(file_content, index)
             case 'i': return BencodeParser.integer(file_content, index)
-            case '1', '2', '3', '4', '5', '6', '7', '8', '9':
+            case '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9':
                 return BencodeParser.byte_string(file_content, index)
         return []
 
     @staticmethod
     def decode(filename):
         with open(filename, "rb") as file: # test_file.torrent
-            file_content = str(file.read())
-            i = 0
-            BencodeParser.stack.push(file_content[0])
-            while (char := file_content[i]) != 'e':
-                # match BencodeParser.stack.peek():
-                #     case 'd': # create a new dictionary
-                returned_value = BencodeParser.dictionary_or_list(file_content[i:], i)
+            file_content = ""
 
-                if BencodeParser.is_key:
-                    BencodeParser.key = returned_value[0]
-                    BencodeParser.is_key = False
-                else:
-                    BencodeParser.is_key = True
-                    BencodeParser.main_dictionary[BencodeParser.key] = returned_value[0]
+            for line in file.read():
+                if line > MAX_PRINTABLE:
+                    break
+                file_content += chr(line)
 
-                i = returned_value[1]
-            else:
-                BencodeParser.stack.pop()
+            returned_value = BencodeParser.structure(file_content, 0)
+
+            print(returned_value)
+
+BencodeParser.decode(input())
