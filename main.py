c6c0b4e1fb5d6b58016a1393190b06f8f5bb662f
Bruno <soubruno@protonmail.com>
2026-02-01 09:14:43 -0300

    refactor: remove from main viewer-like behavior

--- main.py
+++ main.py
@@ -4,15 +4,6 @@
     Date: 24/jan/2026
 """
 
-import sys
-from src.client.bencode import Bencode
+import client
 
-if len(sys.argv) == 2:
-    path = sys.argv[1].replace("\"", '')
-    parsed = Bencode.decode(path)
-elif len(sys.argv) == 1:
-    parsed = Bencode.decode((path := input()).replace("\"",''))
-else:
-    raise TypeError(f"Takes exactly 1 argument. {len(sys.argv) - 1} given.")
-
-print(parsed)
+client.interface()
